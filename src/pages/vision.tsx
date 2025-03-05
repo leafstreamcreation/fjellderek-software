c6abddee86a790c710cf197135406b7abf8b5af6
Derek Noble <shouldergiant@icloud.com>
2025-03-05 03:25:19 -0700

    feat: begin content work on vision page and add link to the skills page

--- src/pages/vision.tsx
+++ src/pages/vision.tsx
@@ -1,13 +1,21 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
 
 export default function VisionPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Vision</h1>
+          <span className={title()}>My&nbsp;</span>
+          <span className={title({ color: "violet" })}>Vision&nbsp;</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            I build tools that help. I design software that puts people first, helping others to gain connection with themselves and others and helping to solve problems that people cannot achieve on their own.
+          </div>
         </div>
+        <Button as={Link} href="/skills" size="lg" radius="full" variant="bordered" color="success">See My Skills</Button>
       </section>
     </DefaultLayout>
   );
