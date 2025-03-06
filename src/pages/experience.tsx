6e3d69b8a9b50db1085981816cd6d82a28b4cc9d
Derek Noble <shouldergiant@icloud.com>
2025-03-06 02:21:14 -0700

    feat: begin outline of experience page and features, add link to the projects page

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,13 +1,21 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
 
 export default function ExperiencePage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Experience</h1>
+          <span className={title()}>My&nbsp;</span>
+          <span className={title({ color: "violet" })}>Experience&nbsp;</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            Here I will place a timeline of my experience from the beggining as a timeline. A search bar will allow the user to seek along the time line and jump to each point along the timeline which the relevant experience.
+          </div>
         </div>
+        <Button as={Link} href="/projects" size="lg" radius="full" variant="bordered" color="success">Next -&gt; Projects</Button>
       </section>
     </DefaultLayout>
   );
