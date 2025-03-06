ba51f27a6b6ea58c62968290dcb13ece825c1fdd
Derek Noble <shouldergiant@icloud.com>
2025-03-06 02:15:34 -0700

    feat: template skills page with link to the experience page

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,13 +1,21 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
 
 export default function SkillsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Skills</h1>
+          <span className={title()}>My&nbsp;</span>
+          <span className={title({ color: "violet" })}>Skills&nbsp;</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            Here I will have a skill table that has skills grouped by stack and visual indicators of proficiency level. The user can select a stack and search for relevant skills. There will also be a default set of displayed skills
+          </div>
         </div>
+        <Button as={Link} href="/experience" size="lg" radius="full" variant="bordered" color="success">See My Experience</Button>
       </section>
     </DefaultLayout>
   );
