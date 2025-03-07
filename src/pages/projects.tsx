c78b6b6cf555968d2643d42972020bf4506e8f02
Derek Noble <shouldergiant@icloud.com>
2025-03-07 01:16:04 -0700

    feat: add placeholder body for the projects page and add a link to the contact page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,21 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
 
 export default function ProjectsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Projects</h1>
+          <span className={title()}>My&nbsp;</span>
+          <span className={title({ color: "violet" })}>Projects&nbsp;</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            Here I will have a project gallery featuring a card for each project with a thumbnail image and an expanding description of the project and a link to the project, if applicable.
+          </div>
         </div>
+        <Button as={Link} href="/contact" size="lg" radius="full" variant="bordered" color="success">My Contact Page -&gt;</Button>
       </section>
     </DefaultLayout>
   );
