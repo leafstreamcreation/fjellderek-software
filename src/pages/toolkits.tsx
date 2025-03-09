b43ba2d5d5bc0545e328f4324ff6096e4b24729c
Derek Noble <shouldergiant@icloud.com>
2025-03-09 16:01:42 -0700

    feat: flesh out toolkits page with content concepts

--- src/pages/toolkits.tsx
+++ src/pages/toolkits.tsx
@@ -1,12 +1,18 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+
 
 export default function ToolkitsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Toolkits</h1>
+      <div className="inline-block max-w-lg text-center justify-center">
+          <span className={title()}>My&nbsp;</span>
+          <span className={title({ color: "green" })}>Toolkits</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            My toolkit page which contains sets of tools I use to develop software. Tools listed will include things like IDE configurations, frameworks, reference sheets, external reference links (like MDN, but also eventually saved internally to enable PWA/offline access), and AI/content generation tools, all of which will be searchable by topic. Topics will concern all areas of software development I have needed tools for.
+          </div>
         </div>
       </section>
     </DefaultLayout>
