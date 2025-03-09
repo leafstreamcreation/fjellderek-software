e4bbc597c8df1ff62adb32c94d4d94b09785ba73
Derek Noble <shouldergiant@icloud.com>
2025-03-09 15:42:08 -0700

    feat: add skeleton/content overview for the blog page and add a link to the toolkits page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,22 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
 
 export default function BlogPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Blog</h1>
+      <div className="inline-block max-w-lg text-center justify-center">
+          <span className={title()}>My&nbsp;</span>
+          <span className={title({ color: "violet" })}>Blog</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            My blog posts which will be searchable and have cards with descriptions which link to a blog page for each post. Topics covered will be tutorials, learning processes, critical analyses of important technologies, and journal posts.    
+          </div>
         </div>
+        <Button as={Link} href="/toolkits" size="lg" radius="full" variant="bordered" color="success">Toolkits -&gt;</Button>
       </section>
     </DefaultLayout>
   );
