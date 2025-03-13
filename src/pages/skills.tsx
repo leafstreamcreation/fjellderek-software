9316f0a5d3dcad6021067c2ff7285af3775892a1
Derek Noble <shouldergiant@icloud.com>
2025-03-12 23:55:07 -0700

    feat: add skill bars and basic skill template information

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -2,6 +2,9 @@ import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import { Button } from "@heroui/button";
 import { Link } from "@heroui/link";
+import { Progress } from "@heroui/react";
+import { skills, Skill } from "@/site-content/skills.ts";
+
 
 import { InProgress } from "@/components/in-progress";
 
@@ -9,6 +12,7 @@ export default function SkillsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+          <InProgress />
         <div className="inline-block max-w-lg text-center justify-center">
           <span className={title()}>My&nbsp;</span>
           <span className={title({ color: "blue" })}>Skills&nbsp;</span>
@@ -16,8 +20,14 @@ export default function SkillsPage() {
           <div className={subtitle({ class: "mt-4" })}>
             Here I will have a skill table that has skills grouped by stack and visual indicators of proficiency level. The user can select a stack and search for relevant skills. There will also be a default set of displayed skills
           </div>
-          <InProgress />
         </div>
+        {
+          skills.map((skill:Skill) => {
+            return (
+              <Progress key={skill.name} value={skill.proficiency}  label={skill.name} size="lg" />
+            );
+          })
+        }
         <Button as={Link} href="/experience" size="lg" radius="full" variant="bordered" color="success">See My Experience</Button>
       </section>
     </DefaultLayout>
---
+++ src/site-content/skills.ts
@@ -0,0 +1,142 @@
+export type Skill = {
+    name: string;
+    proficiency: number;
+    years: number;
+    keys: string[];
+    icon: string;
+};
+
+export const skills = [
+        {
+            name: "React.js",
+            proficiency: 50,
+            years: 2,
+            keys: [
+                "MERN",
+                "Frontend",
+            ],
+            icon: "react",
+        },
+        {
+            name: "JavaScript",
+            proficiency: 90,
+            years: 5,
+            keys: [
+                "ES6",
+                "web",
+                "node",
+            ],
+            icon: "javascript",
+        },
+        {
+            name: "TypeScript",
+            proficiency: 60,
+            years: 2,
+            keys: [
+                "web",
+                "node",
+            ],
+            icon: "typescript",
+        },
+        {
+            name: "HTML",
+            proficiency: 90,
+            years: 5,
+            keys: [
+                "web",
+            ],
+            icon: "html",
+        },
+        {
+            name: "CSS",
+            proficiency: 90,
+            years: 5,
+            keys: [
+                "web",
+            ],
+            icon: "css",
+        },
+        {
+            name: "Tailwind CSS",
+            proficiency: 60,
+            years: 1,
+            keys: [
+                "web",
+            ],
+            icon: "tailwindcss",
+        },
+        {
+            name: "Node.js",
+            proficiency: 90,
+            years: 5,
+            keys: [
+                "backend",
+                "web",
+            ],
+            icon: "nodejs",
+        },
+        {
+            name: "Express.js",
+            proficiency: 85,
+            years: 5,
+            keys: [
+                "backend",
+                "web",
+            ],
+            icon: "express",
+        },
+        {
+            name: "MongoDB",
+            proficiency: 50,
+            years: 1,
+            keys: [
+                "MERN",
+                "backend",
+                "web",
+            ],
+            icon: "mongodb",
+        },
+        {
+            name: "PostgreSQL",
+            proficiency: 40,
+            years: 3,
+            keys: [
+                "backend",
+                "web",
+            ],
+            icon: "postgresql",
+        },
+        {
+            name: "Git",
+            proficiency: 80,
+            years: 15,
+            keys: [
+                "version control",
+            ],
+            icon: "git",
+        },
+        {
+            name: "GitHub",
+            proficiency: 80,
+            years: 15,
+            keys: [
+                "version control",
+            ],
+            icon: "github",
+        },
+        {
+            name: "Jira",
+            proficiency: 50,
+            years: 3,
+            keys: [
+                "project management",
+            ],
+            icon: "jira",
+        },
+];
+
+const keys = {
+    items: [
+
+    ]
+};
