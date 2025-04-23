4293ea931b48f7b35940ee7903fad154d3e50c46
Derek Noble <shouldergiant@icloud.com>
2025-04-22 23:59:24 -0700

    feat: add humor to in progress text

--- src/components/in-progress.tsx
+++ src/components/in-progress.tsx
@@ -17,7 +17,7 @@ export const InProgress = () => {
       </CardHeader>
       <Divider />
       <CardBody>
-        <p className={text()}>This section of the site is under construction and will be available with more content and features later.</p>
+        <p className={text()}>This section of the site is A LEMON and will be available with more content and features later.</p>
       </CardBody>
     </Card>
   );
