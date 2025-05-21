00fa5309479eaae64f57cfb436419a117c6eac45
Derek Noble <shouldergiant@icloud.com>
2025-05-21 11:50:32 -0700

    feat: add complete experience content

--- src/site-content/Experience/experience.ts
+++ src/site-content/Experience/experience.ts
@@ -21,11 +21,18 @@ export const experience: Experience[] = [
       description: "Developed a Unity-based augmented reality radio alignment tool for directed radio antennas, halving the personnel required for alignment.",
       achievements: ["Created automated tests and documentation", "Resolved sensor drift across all devices", "Launched native beta version for iOs and Android"]
     },
-    // {
-    //   period: "2017 - 2018",
-    //   role: "Objective-C Developer",
-    //   company: "Toppan-Merrill",
-    //   description: "Worked on a Unity-based augmented reality radio alignment tool for directed radio antennas, halving the personnel required for alignment.",
-    //   achievements: ["Created automated tests and documentation", "Resolved sensor drift across all devices", "Launched native beta version for iOs and Android"]
-    // }
+    {
+      period: "2017 - 2018",
+      role: "Quark XTensions Developer",
+      company: "Toppan-Merrill",
+      description: "Developed Quark XTensions for automated typesetting and layout of financial documents.",
+      achievements: ["Optimized XTension for bannerlines using Objective-C and PostScript", "Replaced old implementations with clean and maintainable code", "Resolved bugs in legacy codebase and improved performance"]
+    },
+    {
+      period: "2009 - 2010",
+      role: "QT Game Developer",
+      company: "Hazeron Freehold LLC",
+      description: "Developed procedural weather generation systems for a multiplayer space exploration game.",
+      achievements: ["Rendered realistic clouds with temperature, pressure, and humidity topology models", "Created a topology visualizer to accelerate iteration", "Enhanced the weather system to support planets and moons with extreme atmospheres"]
+    }
 ];
